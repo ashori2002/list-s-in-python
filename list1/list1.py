8d8c6b1bdd97de1239ed4e32c2298195f633d465
mohammad ashori <mohammadashori2002@gmail.com>
2022-12-23 13:50:23 +0330

    delete command...

--- list1/list1.py
+++ list1/list1.py
@@ -7,9 +7,12 @@ print(myList[1])
 # Python's append() function inserts a single element into an existing list.
 # The element will be added to the end of the old list rather than being returned to a new list.
 # Adds its argument as a single element to the end of a list.
-
 myList.append("world")
 print(myList)
 
+#The insert() method inserts the specified value at the specified position.
 myList.insert(2, "world")
+print(myList)
+
+del myList[1]
 print(myList)
