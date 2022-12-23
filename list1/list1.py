053c37eca1c32923fde307591dd515cfb6c5770e
mohammad ashori <mohammadashori2002@gmail.com>
2022-12-23 13:24:39 +0330

    Add project files.

---
+++ list1/list1.py
@@ -0,0 +1,12 @@
+#list
+
+myList = ["mohammad" , "ashori" , 2002 , 1.78 , "iran"]
+
+print(myList[1])
+
+#Python's append() function inserts a single element into an existing list.
+# The element will be added to the end of the old list rather than being returned to a new list.
+# Adds its argument as a single element to the end of a list.
+
+myList.append("world")
+print(myList)
